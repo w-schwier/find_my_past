4de5fcb2c2eb92889f9d1c76cc5ca56cd43482ae
Will Schwier <w.schwier@hotmail.co.uk>
2017-10-30 11:00:47 +0000

    Rewrite tests for inputValidator

--- spec/inputValidatorSpec.js
+++ spec/inputValidatorSpec.js
@@ -1,35 +1,46 @@
-(function() {
-  var inputValidator = new InputValidator();
-  inputValidator.check(3)
-  assert.isTrue(inputValidator.check(3))
-})();
+(function(){
 
-(function() {
   var inputValidator = new InputValidator();
-  inputValidator.check(3)
-  assert.isTrue(inputValidator.check(2))
-})();
 
-(function() {
-  var inputValidator = new InputValidator();
-  inputValidator.check(3)
-  assert.isTrue(inputValidator.check(2000))
-})();
+  (function() {
+    assert.isTrue(inputValidator.check(1))
+  })();
 
-(function() {
-  var inputValidator = new InputValidator();
-  inputValidator.check(3)
-  assert.isFalse(inputValidator.check(0))
-})();
+  (function() {
+    assert.isTrue(inputValidator.check(4))
+  })();
 
-(function() {
-  var inputValidator = new InputValidator();
-  inputValidator.check(3)
-  assert.isFalse(inputValidator.check(0.6))
-})();
+  (function() {
+    assert.isFalse(inputValidator.check(0))
+  })();
 
-(function() {
-  var inputValidator = new InputValidator();
-  inputValidator.check(3)
-  assert.isFalse(inputValidator.check(5.5))
+  // (function() {
+  //   var inputValidator = new InputValidator();
+  //   inputValidator.check(3)
+  //   assert.isTrue(inputValidator.check(2))
+  // })();
+  //
+  // (function() {
+  //   var inputValidator = new InputValidator();
+  //   inputValidator.check(3)
+  //   assert.isTrue(inputValidator.check(2000))
+  // })();
+  //
+  // (function() {
+  //   var inputValidator = new InputValidator();
+  //   inputValidator.check(3)
+  //   assert.isFalse(inputValidator.check(0))
+  // })();
+  //
+  // (function() {
+  //   var inputValidator = new InputValidator();
+  //   inputValidator.check(3)
+  //   assert.isFalse(inputValidator.check(0.6))
+  // })();
+  //
+  // (function() {
+  //   var inputValidator = new InputValidator();
+  //   inputValidator.check(3)
+  //   assert.isFalse(inputValidator.check(5.5))
+  // })();
 })();
---
+++ src/inputValidator.js
@@ -0,0 +1,9 @@
+(function(exports) {
+  function InputValidator() {
+    this.check = function(x) {
+
+    };
+  };
+
+  exports.InputValidator = InputValidator;
+})(this);
