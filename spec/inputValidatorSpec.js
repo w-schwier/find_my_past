39c0666f1bd3a669d512e52fbf50476c6549796d
Will Schwier <w.schwier@hotmail.co.uk>
2017-10-30 11:07:00 +0000

    Add tests for decimals in inputValidator

--- spec/inputValidatorSpec.js
+++ spec/inputValidatorSpec.js
@@ -14,33 +14,29 @@
     assert.isFalse(inputValidator.check(0))
   })();
 
-  // (function() {
-  //   var inputValidator = new InputValidator();
-  //   inputValidator.check(3)
-  //   assert.isTrue(inputValidator.check(2))
-  // })();
-  //
-  // (function() {
-  //   var inputValidator = new InputValidator();
-  //   inputValidator.check(3)
-  //   assert.isTrue(inputValidator.check(2000))
-  // })();
-  //
-  // (function() {
-  //   var inputValidator = new InputValidator();
-  //   inputValidator.check(3)
-  //   assert.isFalse(inputValidator.check(0))
-  // })();
-  //
-  // (function() {
-  //   var inputValidator = new InputValidator();
-  //   inputValidator.check(3)
-  //   assert.isFalse(inputValidator.check(0.6))
-  // })();
-  //
-  // (function() {
-  //   var inputValidator = new InputValidator();
-  //   inputValidator.check(3)
-  //   assert.isFalse(inputValidator.check(5.5))
-  // })();
+  (function() {
+    assert.isTrue(inputValidator.check(2))
+  })();
+
+  (function() {
+    assert.isTrue(inputValidator.check(2000))
+  })();
+
+  (function() {
+    assert.isFalse(inputValidator.check(0.6))
+  })();
+
+  (function() {
+    assert.isFalse(inputValidator.check(5.5))
+  })();
+
+  (function() {
+    assert.isFalse(inputValidator.check(55.5))
+  })();
+
+  (function() {
+    assert.isFalse(inputValidator.check("f"))
+  })();
+
+
 })();
