5ac856ef7bfed0a595e819dbeda30eebcc1ca4f9
Will Schwier <w.schwier@hotmail.co.uk>
2017-10-30 18:40:50 +0000

    Add test for multiplyTable in tableGenerator

--- spec/tableGeneratorSpec.js
+++ spec/tableGeneratorSpec.js
@@ -6,4 +6,10 @@
     assert.arraysEqual(tableGenerator.finishedTable, [])
   })();
 
+  (function() {
+    var testArray = [2, 3, 5]
+    var multiArray = [['x', 2, 3, 5], [2, 4, 6, 10], [3, 6, 9, 15], [5, 10, 15, 25]]
+    assert.arraysEqual(tableGenerator.multiplyArray(testArray), multiArray);
+  })();
+
 })();
