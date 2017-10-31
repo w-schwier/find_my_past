248caabcb38e13affaad0afcedd26d989dba2dbc
Will Schwier <w.schwier@hotmail.co.uk>
2017-10-31 12:26:10 +0000

    Add test for multiplyArray

--- src/tableGenerator.js
+++ src/tableGenerator.js
@@ -3,5 +3,19 @@
     this.finishedTable = [];
   };
 
+  TableGenerator.prototype.createTable = function () {
+
+  };
+  // 
+  // TableGenerator.prototype.populateTable = function (array) {
+  //   var multiArray = [];
+  //   var size = array.length;
+  //   for (i = 0; i < size; ++) {
+  //
+  //   };
+  //   return multiArray;
+  //
+  // };
+
   exports.TableGenerator = TableGenerator;
 })(this);
