6f54da86369f9e3a0c1c1c8e7664dce22ced8a56
Will Schwier <w.schwier@hotmail.co.uk>
2017-11-03 18:19:59 +0000

    Add test for makeTable in tableGenerator

--- spec/tableGeneratorSpec.js
+++ spec/tableGeneratorSpec.js
@@ -13,8 +13,16 @@
     var multiples = [[4, 6, 10], [6, 9, 15], [10, 15, 25]]
     var multiArray = [["X", 2, 3, 5], [2, 4, 6, 10], [3, 6, 9, 15], [5, 10, 15, 25]]
     assert.multiArraysEqual(tableGenerator.formatArray(testArray, multiples), multiArray);
-    })();
+  })();
+
+  (function() {
+    var multiArray = [["X", 2, 3, 5], [2, 4, 6, 10], [3, 6, 9, 15], [5, 10, 15, 25]]
+    var table = "\
+      X  | [2] | [3] | [5] \
+     [2] |  4  |  6  | 10 \
+     [3] |  6  |  9  | 15 \
+     [5] | 10  | 15  | 25 "
+    assert.isEqual(tableGenerator.makeTable(multiArray), table);
+  })();
 
 })();
-
-// var finishedTable = "X [2][3][5]\n[2] 4 6 10 \n[3] 6 9 15 \n[5] 10 15 25 \n"
