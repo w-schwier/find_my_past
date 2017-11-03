8f9407a596ed411f836bffcccfe096206323dcf2
Will Schwier <w.schwier@hotmail.co.uk>
2017-11-03 16:10:10 +0000

    Add test for formatArray in tableGenerator

--- spec/tableGeneratorSpec.js
+++ spec/tableGeneratorSpec.js
@@ -19,11 +19,12 @@
     assert.multiArraysEqual(tableGenerator.multiplyArray(testArray), multiArray);
   })();
 
-  // (function() {
-  //   var testArray = [2, 3, 5]
-  //   var finishedTable = "X [2][3][5]\n[2] 4 6 10 \n[3] 6 9 15 \n[5] 10 15 25 \n"
-  //   assert.isEqual(tableGenerator.multiplyArray2(testArray), finishedTable);
-  //   })();
+  (function() {
+    var testArray = [2, 3, 5]
+    var multiArray = [["X", 2, 3, 5], [2, 4, 6, 10], [3, 6, 9, 15], [5, 10, 15, 25]]
+    assert.multiArraysEqual(tableGenerator.formatArray(testArray), multiArray);
+    })();
 
 })();
-// var multiArray = [["X", 2, 3, 5], [2, 4, 6, 10], [3, 6, 9, 15], [5, 10, 15, 25]]
+
+// var finishedTable = "X [2][3][5]\n[2] 4 6 10 \n[3] 6 9 15 \n[5] 10 15 25 \n"
--- src/tableGenerator.js
+++ src/tableGenerator.js
@@ -12,7 +12,7 @@
     var multiples = [];
     var size = primes.length;
     for (i = 0; i < size; i++) {
-      multiArray[i] = [];
+      multiples[i] = [];
       for(j = 0; j < size; j++){
         multiples[i][j] = primes[i] * primes[j];
       };
@@ -20,25 +20,7 @@
     return multiples;
   };
 
-  TableGenerator.prototype.multiplyArray2 = function (primes) {
-    var result = 'X   ';
-    var headingArray = this.formatArray(primes)
-    var size = primes.length;
-    for (i = 0; i < size; i++) {
-      for(j = 0; j < size; j++){
-        if (i == 0 && j > 0 ) {
-          result += '[' + headingArray[j] + ']' + buff((headingArray[j]+'').length+2);
-        } else if (j == 0 && i > 0) {
-          result += '[' + headingArray[i] + '] ';
-        } else if (i>0 && j>0){
-          var value = (primes[i]*primes[j])
-          result += buff((value+'').length ) + value;
-        };
-      };
-      result += '\n'
-    };
-    return result;
-  };
+
 
   exports.TableGenerator = TableGenerator;
 })(this);
@@ -51,23 +33,22 @@ function buff(val){
   return buff;
 }
 
-// TableGenerator.prototype.multiplyArray = function (primes) {
-//   var multiArray = [];
+// TableGenerator.prototype.multiplyArray2 = function (primes) {
+//   var result = 'X   ';
 //   var headingArray = this.formatArray(primes)
 //   var size = primes.length;
 //   for (i = 0; i < size; i++) {
-//     multiArray[i] = [];
 //     for(j = 0; j < size; j++){
-//       if (i == 0) {
-//         multiArray[i][j] = headingArray[j]
-//       } else if (j == 0) {
-//         multiArray[i][j] = headingArray[i]
-//       } else {
-//         prime = primes[i]
-//         prime2 = primes[j]
-//         multiArray[i][j] = prime * prime2;
+//       if (i == 0 && j > 0 ) {
+//         result += '[' + headingArray[j] + ']' + buff((headingArray[j]+'').length+2);
+//       } else if (j == 0 && i > 0) {
+//         result += '[' + headingArray[i] + '] ';
+//       } else if (i>0 && j>0){
+//         var value = (primes[i]*primes[j])
+//         result += buff((value+'').length ) + value;
 //       };
 //     };
+//     result += '\n'
 //   };
-//   return multiArray;
+//   return result;
 // };
